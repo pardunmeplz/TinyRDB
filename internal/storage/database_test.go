a0997c9bfa4cf8f7eb6f9d00b285d277057ec5aa
Parth Dundh <dundh.parth@gmail.com>
2025-04-11 00:40:37 +0530

    Adding test cases for shutdown, checkpoints, rewrites, partial page writes

--- internal/storage/database_test.go
+++ internal/storage/database_test.go
@@ -61,6 +61,42 @@ func TestPageWriteAndRecovery(t *testing.T) {
 		pageData[id] = data
 	}
 
+	{
+		// rewrite on last page to test durability on multiple writes for same page
+		data := MakePageData()
+		rand.Read(data[:])
+		_, err := DatabaseManager.WritePages([]PageDelta{
+			{
+				pageIDs[4],
+				0,
+				data[:],
+			},
+		})
+		if err != nil {
+			t.Fatal("Write failed for page", pageIDs[4], ":", err)
+		}
+		pageData[pageIDs[4]] = data
+	}
+
+	{
+		// partial rewrite on 4th page to test partial writes
+		data := make([]byte, 15)
+		rand.Read(data[:])
+		_, err := DatabaseManager.WritePages([]PageDelta{
+			{
+				pageIDs[3],
+				50,
+				data[:],
+			},
+		})
+		if err != nil {
+			t.Fatal("Write failed for page", pageIDs[3], ":", err)
+		}
+		for i, byteData := range data {
+			pageData[pageIDs[3]][50+i] = byteData
+		}
+	}
+
 	// Read back and verify the data
 	for _, id := range pageIDs {
 		readData, err := DatabaseManager.getPage(id)
@@ -169,4 +205,71 @@ func TestCacheEviction(t *testing.T) {
 	if ok {
 		t.Fatal("Page 0 was not removed from cache")
 	}
+
+	readPage, err := DatabaseManager.getPage(pageIDs[0])
+	if err != nil {
+		t.Fatal("Failed to read page ", err)
+	}
+	if string(readPage[:]) != string(pageData[pageIDs[0]][:]) {
+		t.Error("Data mismatch for page", pageData[pageIDs[0]])
+	}
+}
+
+func TestCheckpointTrigger(t *testing.T) {
+	os.Remove("test.log")
+	os.Remove("test.db")
+	checkpointTrigger := 10000
+	DatabaseManager := newDatabase(t, uint64(checkpointTrigger), 32000)
+	defer DatabaseManager.Shutdown()
+
+	defer DatabaseManager.Shutdown()
+
+	// allocate some pages
+	PageCount := 5
+	pageIDs := []uint64{}
+	for i := 0; i < PageCount; i++ {
+		pageID, err := DatabaseManager.allocator.AllocatePage(PagetypeUserdata)
+		if err != nil {
+			t.Fatal("Page allocation failed:", err)
+		}
+		pageIDs = append(pageIDs, pageID)
+	}
+
+	// Write random data to pages
+	pageData := make(map[uint64]PageData)
+	for _, id := range pageIDs {
+		data := MakePageData()
+		rand.Read(data[:])
+		_, err := DatabaseManager.WritePages([]PageDelta{
+			{
+				id,
+				0,
+				data[:],
+			},
+		})
+		if err != nil {
+			t.Fatal("Write failed for page", id, ":", err)
+		}
+		pageData[id] = data
+	}
+
+	stats, err := DatabaseManager.wal.Log.Stat()
+	if err != nil {
+		t.Fatal("Page Stat failed :", err)
+	}
+	walSize := stats.Size()
+	t.Log(walSize)
+	if walSize > int64(checkpointTrigger) {
+		t.Fatal("Wal Truncation failed :", err)
+	}
+
+	readData, err := DatabaseManager.allocator.ReadPageData(pageIDs[0])
+	if err != nil {
+		t.Fatal("Page read failed  :", err)
+	}
+
+	if string(readData[:]) != string(pageData[pageIDs[0]][:]) {
+		t.Error("Data mismatch during transfer to disk at page ", pageIDs[0])
+	}
+
 }
